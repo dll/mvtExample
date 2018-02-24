a8ed34cfce7f1da61a59a9e07c3b2de899996e36
sennahLessert <sennahLessert@tutanota.com>
2018-02-24 21:24:59 +0100

    add db and retrieve some geometries from rnd database

---
+++ db.js
@@ -0,0 +1,17 @@
+const { Client } = require("pg");
+
+function Db(){
+				this.client = new Client("postgres://postgres:postgres@localhost:5432/geodb");
+				this.client.connect();
+}
+
+Db.prototype.getVectorTiles = function(){
+				var me = this;
+				return new Promise((resolve, reject)=>{
+								me.client.query("select geom from geoms limit 200;", (error, results)=>{
+												resolve(results.rows);
+								});
+				});
+};
+
+module.exports = Db;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var jade = require("jade");
 var express = require("express");
+var Db = require("./db");
 
 var app = express();
 app.set("view engine", "jade")
@@ -8,8 +9,13 @@ app.use(express.static("js"))
 
 jade.renderFile("views/index.jade")
 
-app.get("/", function(req, res){
-    res.render('index');
+app.get("/", (req, res)=>{
+				var db = new Db();
+				db.getVectorTiles().then(response=>{
+								res.render('index', {
+												data: response
+								});
+				});
 });
 
-app.listen("8181", function(){});
+app.listen("8181", ()=>{});
