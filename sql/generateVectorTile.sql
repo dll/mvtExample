051373483ba9076b1cee022abcdce282ac73108f
sennahLessert <sennahLessert@tutanota.com>
2018-02-25 21:09:02 +0100

    concept for creating vector tiles via postgis

---
+++ sql/generateVectorTile.sql
@@ -0,0 +1,17 @@
+with geom as (
+	select geom as geom from geoms where id = 1
+), bottomleft as (
+	select st_setsrid(st_point(st_xmin(geom), st_ymin(geom)), 4326) as geom
+	from geom
+), topright as (
+	select st_setsrid(st_point(st_xmax(geom), st_ymax(geom)), 4326) as geom
+	from geom
+), boundingbox as (
+	select st_makebox2d((select geom from bottomleft), (select geom from topright)) as bbox
+)
+
+select st_asmvt(x)
+from (
+	select st_asmvtgeom((select geom from geom), bbox, 4096, 0, false)
+	from boundingbox
+)x;
